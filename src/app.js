81ef2312865b9f3cfd3e108fb456064f6d21f471
newton <chiakajunior@yahoo.com>
2020-05-04 20:31:02 +0100

    created the backend environment

--- src/app.js
+++ src/app.js
@@ -1,3 +1,22 @@
-const app = async () => '#BuildforSDG';
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const app = express();
+app.use(cors());
+require('dotenv').config();
 
-export default app;
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri,{useNewUrlParser:true, useCreateIndex: true,  useUnifiedTopology: true})
+  .then(() => {
+    console.log('Successfully connected to MongoDB Atlas!');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!');
+    console.error(error);
+  });
+
+app.use((req, res) => {
+    res.json({message:"your request wass successfull"});
+});
+
+module.exports = app;
