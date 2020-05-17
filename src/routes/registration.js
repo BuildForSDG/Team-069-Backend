2e4a099e3fac238e7e665fd48a717fa2e4192395
newton <chiakajunior@yahoo.com>
2020-05-17 02:49:04 +0100

    added routes and models folder

--- src/routes/registration.js
+++ src/routes/registration.js
@@ -1,18 +1,19 @@
+/* eslint-disable linebreak-style */
 /* eslint-disable padded-blocks */
 /* eslint-disable eol-last */
 /* eslint-disable linebreak-style */
 const router = require('express').Router();
 
-const signup = require('../models/registration');
+const Signup = require('../models/registration');
 
-router.post('/add', (req, res, next) => {
+router.post('/add', (req, res) => {
 
-  const newsignup = new signup ({
-    userame: req.body.username
+  const newsignup = new Signup({
+    username: req.body.username
   });
   newsignup.save()
     .then(() => res.json('user registered'))
-    .catch((err) => res.status(400).json({error: err}))
+    .catch((err) => res.status(400).json({ error: err }));
 });
 
 module.exports = router;
